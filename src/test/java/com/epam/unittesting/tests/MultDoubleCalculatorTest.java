c02094de7990bece05ad497ec33483e12bdf3fd6
Yuriy_Jurayev <yuriy_jurayev@epam.com>
2017-11-17 00:13:16 +0600

    deleted comments

--- src/test/java/com/epam/unittesting/tests/MultDoubleCalculatorTest.java
+++ src/test/java/com/epam/unittesting/tests/MultDoubleCalculatorTest.java
@@ -10,6 +10,6 @@ public class MultDoubleCalculatorTest extends BaseTest {
     public void multTest(double a, double b, double expected){
         double result = calc.mult(a, b);
         System.out.println("multiplication: " + a + " * " + b + " = " + result);
-        Assert.assertEquals(result, expected, "Invalid result of multiplication: ");  /// bug - Math.floor is make @return  the closest floating-point value to {@code a} that is equal to a mathematical integer.
+        Assert.assertEquals(result, expected, "Invalid result of multiplication: ");
     }
 }
--- src/test/java/com/epam/unittesting/tests/SinCalculatorTest.java
+++ src/test/java/com/epam/unittesting/tests/SinCalculatorTest.java
@@ -8,7 +8,7 @@ public class SinCalculatorTest extends BaseTest {
 
     @Test(dataProvider = "sin", dataProviderClass = CalculatorTestDataProvider.class)
     public void sinTest(double a, double expected) {
-        double result = calc.sin(Math.toRadians(a));  ///convert degrees to radians
+        double result = calc.sin(Math.toRadians(a));
         System.out.println("sin of " + a + " degree" + " = " + result);
         Assert.assertEquals(result, expected, "Invalid result of sin: ");
     }
