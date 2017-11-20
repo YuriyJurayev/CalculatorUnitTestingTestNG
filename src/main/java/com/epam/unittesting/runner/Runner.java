bb86a5a18ceccea73ab6e6065f556402b275f485
Yuriy_Jurayev <yuriy_jurayev@epam.com>
2017-11-20 13:13:53 +0600

    temporary changes

--- src/main/java/com/epam/unittesting/runner/Runner.java
+++ src/main/java/com/epam/unittesting/runner/Runner.java
@@ -6,10 +6,24 @@ import org.testng.TestNG;
 import org.testng.xml.Parser;
 import org.testng.xml.XmlSuite;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Runner {
-    public static void main(String[] args) throws RunnerException {
+    public static void main(String[] args){
+        TestNG testng = new TestNG();
+
+        List<String> suites = new ArrayList<>();
+        suites.add("scenarios/testng.xml");
+        suites.add("scenarios/testnggrouped.xml");
+        suites.add("scenarios/testngparallel.xml");
+        suites.add("scenarios/testngparameterized.xml");
+        testng.setTestSuites(suites);
+        testng.setOutputDirectory("target/test-output");
+        testng.run();
+    }
+
+    /*public static void main(String[] args) throws RunnerException {
 
         System.out.println("Runner");
         TestNG testng = new TestNG();
@@ -23,5 +37,5 @@ public class Runner {
         } catch (IOException e) {
             throw new RunnerException("Ошибка при чтении" + e.getMessage());
         }
-    }
+    }*/
 }
