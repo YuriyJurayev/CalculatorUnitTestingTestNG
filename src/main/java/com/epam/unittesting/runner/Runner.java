c2b4cad986621250457da9e6c4344c7460805a96
Yuriy_Jurayev <yuriy_jurayev@epam.com>
2017-11-20 15:56:35 +0600

    deleted old runner logic

--- src/main/java/com/epam/unittesting/runner/Runner.java
+++ src/main/java/com/epam/unittesting/runner/Runner.java
@@ -17,20 +17,4 @@ public class Runner {
         testng.setOutputDirectory("target/test-output");
         testng.run();
     }
-
-    /*public static void main(String[] args) throws RunnerException {
-
-        System.out.println("Runner");
-        TestNG testng = new TestNG();
-        String xmlFileName = "testng.xml";
-        List<XmlSuite> suite;
-        try {
-            suite = (List<XmlSuite>) (new Parser(xmlFileName).parse());
-            testng.setXmlSuites(suite);
-            testng.run();
-
-        } catch (IOException e) {
-            throw new RunnerException("Ошибка при чтении" + e.getMessage());
-        }
-    }*/
 }
